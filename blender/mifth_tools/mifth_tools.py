0887a89682d5a331bf760912f75aff0cb3ad223c
mifth <paulgeraskin@gmail.com>
2015-01-16 02:26:30 +0300

    remove unused stuff

--- blender/mifth_tools/mifth_tools.py
+++ blender/mifth_tools/mifth_tools.py
@@ -275,15 +275,3 @@ class MFTMorfCreator(bpy.types.Operator):
                             {'INFO'}, "Model " + obj.name + " has different points count")
 
         return {'FINISHED'}
-
-
-def register():
-    bpy.utils.register_module(__name__)
-
-
-def unregister():
-    bpy.utils.unregister_module(__name__)
-
-
-if __name__ == "__main__":
-    register()
