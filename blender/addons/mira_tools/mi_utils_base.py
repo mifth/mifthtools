d434a8a70f63e7fa23cfd0b1d90a17e0d2794484
mifth <paulgeraskin@gmail.com>
2016-03-22 01:44:57 +0300

    fix for blender 2.77 picking system. For Mifth and Mira.

--- blender/addons/mira_tools/mi_utils_base.py
+++ blender/addons/mira_tools/mi_utils_base.py
@@ -95,9 +95,9 @@ def obj_raycast(obj, matrix, view_vector, ray_origin, ray_max):
     ray_target_obj = matrix_inv * ray_target
 
     # cast the ray
-    hit, normal, face_index = obj.ray_cast(ray_origin_obj, ray_target_obj)
+    hit_result, hit, normal, face_index = obj.ray_cast(ray_origin_obj, ray_target_obj)
 
-    if hit is not None:
+    if hit_result:
         hit_world = matrix * hit
 
         length_squared = (hit_world - ray_origin).length_squared
